fdb542050d5064c57c24818a4f78447aee6c950f
akhilaXD <akhilacreddy@gmail.com>
2026-03-11 18:04:31 +0530

    Assignment -3 (Arrays/Datatypes/Operators)

--- assignments/array.ts
+++ assignments/array.ts
@@ -1,23 +1,23 @@
-let studentNames:string[]=["Suresh","Mahesh","Naresh"];
-let studentMarks:number[]=[75, 80, 82];
+let studentNames: string[] = ["Suresh", "Mahesh", "Naresh"];
+let studentMarks: number[] = [75, 80, 82];
 
-let updatedStudentMarks:number[]=[];
+let updatedStudentMarks: number[] = [];
 
-let averageMarks:number =0;
-let totalMarks:number=0;
+let averageMarks: number = 0;
+let totalMarks: number = 0;
 
 //updating student marks by adding 10
-for(let i=0; i<studentMarks.length;i++){
-    updatedStudentMarks[i]= studentMarks[i]+10;
-    totalMarks+=updatedStudentMarks[i];
+for (let i = 0; i < studentMarks.length; i++) {
+    updatedStudentMarks[i] = studentMarks[i] + 10;
+    totalMarks += updatedStudentMarks[i];
 }
 
-averageMarks=totalMarks/studentMarks.length;
+averageMarks = totalMarks / studentMarks.length;
 
 //printing updated marks
 console.log("Updated Marks:")
-for(let i=0; i<studentMarks.length;i++){
-console.log(`${studentNames[i]}:  ${updatedStudentMarks[i]}`);
+for (let i = 0; i < studentMarks.length; i++) {
+    console.log(`${studentNames[i]}:  ${updatedStudentMarks[i]}`);
 }
 
 console.log(`Average Marks: ${averageMarks}`)

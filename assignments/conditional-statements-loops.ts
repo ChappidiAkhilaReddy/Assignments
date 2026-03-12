7d4d10f90f234eba0b09054bda3df311483b6160
akhilaXD <akhilacreddy@gmail.com>
2026-03-12 12:36:14 +0530

    Assignment -4 (Conditional Statements & Loops)

--- assignments/conditional-statements-loops.ts
+++ assignments/conditional-statements-loops.ts
@@ -4,25 +4,30 @@ let transactions:number[]=[50000,-2000,3000,-15000,-200,-300,4000,-3000];
 //Print total number of credit and debit transactions completed
 let debitCount:number=0;
 let creditCount:number=0;
+let debitAmount:number=0;
+let creditAmount:number=0;
+let suspiciousTransactionsCount=0;
+
 for(let i=0; i<transactions.length;i++){
 if(transactions[i]<0){
     debitCount++;
+    debitAmount=debitAmount+(-(transactions[i]));
+    if(transactions[i]< -10000){
+                console.log(`Suspicious debit transcaction : ${transactions[i]}`);
+        suspiciousTransactionsCount++;
+    }
 }else{
-    creditCount++
+    creditCount++;
+    creditAmount=creditAmount+transactions[i];
+    if(transactions[i]>10000){
+        console.log(`Suspicious credit transcaction : ${transactions[i]}`);
+        suspiciousTransactionsCount++;
+    }
 }
 }
 console.log(`Total number of credit transactions are ${creditCount} and debit transactions are ${debitCount}`);
 
 //Print the total amount credited and debited in account
-let debitAmount:number=0;
-let creditAmount:number=0;
-for(let i=0; i<transactions.length;i++){
-if(transactions[i]<0){
-    debitAmount=debitAmount+(-(transactions[i]));
-}else{
-    creditAmount=creditAmount+transactions[i];
-}
-}
 console.log(`Total credit amount is ${creditAmount} and debit amount is ${debitAmount}`);
 
 //Print total amount remaining at the end in Bank Account
@@ -31,19 +36,7 @@ console.log(`Total credit amount is ${creditAmount} and debit amount is ${debitA
 
  /*If any transaction limit exceeds +/- 10000 then print the message “Suspicious credit/ debit
 Transaction with Amount” and also print total number of suspicious transactions*/
-let suspiciousTransactionsCount=0;
-for(let i=0; i<transactions.length;i++){
-    if(transactions[i]>10000){
-        console.log(`Suspicious credit transcaction : ${transactions[i]}`);
-        suspiciousTransactionsCount++;
-    }
-    else if(transactions[i]< -10000){
-        console.log(`Suspicious debit transcaction : ${transactions[i]}`);
-        suspiciousTransactionsCount++;
-    }
-}
-
-console.log(`Total suspicious transcaction count ${suspiciousTransactionsCount}`);
+console.log(`Total suspicious transaction count ${suspiciousTransactionsCount}`);
 
 
  
